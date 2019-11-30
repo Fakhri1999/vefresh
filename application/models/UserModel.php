72f994f3a36cf8d3f95b82ea591fa00981ef6c5b
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2019-11-30 23:36:05 +0700

    fix error model

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class UserModel extends CI_Model
 {
 	public function ambilDataUser($where){
-		return $this->db->get('user', $where)->row_array();
+		return $this->db->get_where('user', $where)->row_array();
 	}
 
 	public function ambilDataKelurahan(){
